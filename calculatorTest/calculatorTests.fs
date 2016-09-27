ec4b66efaafbaf4de539f904123828df00a9aa24
Ernesto Pérez Nieto <ernesto.pereznieto@abcam.com>
2016-09-27 09:05:52 +0100

    added a new test case

--- calculatorTest/calculatorTests.fs
+++ calculatorTest/calculatorTests.fs
@@ -7,3 +7,9 @@ module calculatorTest =
     [<Fact>]
     let ``When 2 is added to 3 you get 5``() =
         Assert.Equal(5, add 2 3)
+
+    [<Fact>]
+    let ``When always return from clause f# services and the value passed``() =
+        let result = value 3 
+        Assert.Equal(3, result.value)
+        Assert.Equal("F# services", result.from)
